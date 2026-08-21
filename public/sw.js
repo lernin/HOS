b7b9e9e7dcc79e04dcd65175b17ca14aa29f5d42
lernin <ashley.benson.tait@gmail.com>
2026-08-22 00:26:01 +0900

    Use Thekonym as installed app name

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'thekonym-standalone-v1'
+const CACHE = 'thekonym-standalone-v2'
 const SHELL = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/icon-maskable-512.png']
 
 self.addEventListener('install', (event) => {
